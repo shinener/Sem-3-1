0124dd22199cd90f9b621ee2f93d14536fa0d6f0
shinener <Shinehayag17@gmail.com>
2025-02-18 03:53:20 +0800

    new files

---
+++ Main.py
@@ -0,0 +1,11 @@
+import function1
+
+a = int(input("Ehnii too: "))
+b = int(input("Hoyordoh too "))
+
+
+print("a + b =", function1.add(a, b))
+
+d = int(input("Ehnii too: "))
+e = int(input("Hoyordoh too "))
+print("d - e =", function1.add(d, e))
---
+++ function1.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    c = a + b
+    return c
+def add(d, e):
+    f = d - e
+    return f
+    
+    
